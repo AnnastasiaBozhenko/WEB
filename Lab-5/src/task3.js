1e526dfc831048dd0bf45f6690e14c942ba5456d
Anastasiya <anastasiyaBozhenko@gmail.com>
2020-03-30 16:46:41 +0300

    add return

--- Lab-5/src/task3.js
+++ Lab-5/src/task3.js
@@ -13,7 +13,7 @@ function additivePersistence(number) {
         if( res < 10){
             // console.log("resalt " + res);
             console.log("It takes "+ count + ' iterations to reach a single-digit number' );
-            break;
+            return count;
         }
         val = String(res).split('');
         res = 0;
@@ -23,21 +23,25 @@ function multiplicativePersistence(number) {
     let val = String(number).split('');
     let res = 1;
     let count = 0;
-    while (true){
-        for(let i = 0; i < val.length; i++){
+    while (true) {
+        for (let i = 0; i < val.length; i++) {
             res *= Number(val[i]);
-            if (i == val.length-1 ){ console.log("resalt " + res);}
+            if (i == val.length - 1) {
+                console.log("resalt " + res);
+            }
 
         }
         count++;
-        if( res < 10){
-            console.log("resalt " + res);
-            console.log("It takes "+ count + ' iterations to reach a single-digit number' );
-            break;
+        if (res < 10) {
+            // console.log("resalt " + res);
+            console.log("It takes " + count + ' iterations to reach a single-digit number');
+            return count;
         }
         val = String(res).split('');
         res = 1;
     }
 }
+console.log("input:1679583");
 additivePersistence(1679583);
-multiplicativePersistence(77);
+console.log("4996238671872");
+multiplicativePersistence(4996238671872);
